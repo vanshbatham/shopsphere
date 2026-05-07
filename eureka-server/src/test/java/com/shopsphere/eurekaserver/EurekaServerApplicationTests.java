9426ddf2158b23441ff8ff58c1541d069876c98d
vanshbatham <vanshbatham.pro@gmail.com>
2026-05-07 22:26:18 +0530

    feat(eureka-server): implement service discovery registry on port 8761

---
+++ eureka-server/src/test/java/com/shopsphere/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsphere.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
